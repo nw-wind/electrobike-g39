579dd1f0ac6d0adf610dcd39bf8be95351edf1ed
skeler <sergei@keler.net>
2017-11-20 00:15:30 +0300

    consts

--- electrobike-g39.ino
+++ electrobike-g39.ino
@@ -1,22 +1,39 @@
 /* Garage 39 project
- Pins D:
- 2 Датчик скорости (холла)
- 3
- 4 Индикатор
- 5 Направление: подъём/опускание
- 6 Старт/стоп подвески
- 7 Вибрация в ручках
- 
- Pins A:
- 0 Датчик газа
- 1 Управление газом
- 2 Управление тормозом
- 
- 
+  Pins D:
+  2 Датчик скорости (холла)
+  3
+  4 Индикатор
+  5 Направление: подъём/опускание
+  6 Старт/стоп подвески
+  7 Вибрация в ручках
+
+  Pins A:
+  0 Датчик газа
+  1 Управление газом
+  2 Управление тормозом
+
+
 */
 
+#include "SmartDelay.h"
+
+const byte pinInSpeedSensor = 2;
+const byte pinOutIndicator = 4;
+const byte pinOutDirecton = 5;
+const byte pinOutStartStop = 6;
+const byte pinOutVibro = 7;
+
+const byte pinInGas = 0;
+
+// Таймауты
+const unsigned long toutDownToUp = 250 * 1000; // От стояния внизу до начала подъёма.
+const unsigned long toutTurnOnToReady = 250 * 1000; // От готовности к включению до включения.
+
+SmartDelay dDownToUp(toutDownToUp);
+SmartDelay dTurnOnToReady(toutTurnOnToReady);
 
 void setup() {
+
 }
 
 void loop() {
